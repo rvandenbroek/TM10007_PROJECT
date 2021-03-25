f8f670f9acaed0d9845e5b3037e7d9043fae6a89
Edwin Klinkenberg <edwin.klinkenberg@gmail.com>
2021-03-25 12:21:07 +0100

    Co-authored-by: BramRoumen <BramRoumen@users.noreply.github.com>

--- scaler.py
+++ scaler.py
@@ -12,6 +12,12 @@ from sklearn import preprocessing
 from sklearn import neighbors
 from sklearn import svm
 
+# Other scalers 
+# preprocessing.MinMaxScaler() --> scale from 0 1 
+# preprocessing.MaxAbsScaler() --> -1 to 1
+# RobustScaler as a drop-in replacement
+# RobustScaler(*, with_centering=True, with_scaling=True, quantile_range=25.0, 75.0, copy=True, unit_variance=False)
+
 
 # Scale the dataset
 scaler = preprocessing.StandardScaler()
