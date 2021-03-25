25b6cfd1dd5e66bbb9a9db2a6f76fba588ba1fa3
joycerijs <j.rijs@student.tudelft.nl>
2021-03-25 12:16:16 +0100

    Added robust_scaler
    Co-authored-by: rvandenbroek <rvandenbroek@users.noreply.github.com>

--- scaler.py
+++ scaler.py
@@ -1,33 +1,14 @@
-# General packages
+# Scaler
+
+
 import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import datasets as ds
-import seaborn
-
-from sklearn import model_selection
-from sklearn import metrics
-from sklearn import feature_selection 
 from sklearn import preprocessing
-from sklearn import neighbors
-from sklearn import svm
 
-
-# Scale the dataset
-scaler = preprocessing.StandardScaler()
-scaler.fit(X)
-X_scaled = scaler.transform(X)
-
-
-clf_knn = neighbors.KNeighborsClassifier(n_neighbors=5)
-clf_knn.fit(X_scaled, y)
-
-# Test the classifier on the training data and plot
-score_train = clf_knn.score(X_scaled, y)
-
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(1, 1, 1)
-ax.set_title(f"Training performance: accuracy {score_train}")
-colorplot(clf_knn, ax, X_scaled[:, 0],X_scaled[:, 1], h=1000)
-ax.scatter(X_scaled[:, 0], X_scaled[:, 1], marker='o', c=y,
-           s=25, edgecolor='k', cmap=plt.cm.Paired)
+def robust_scaler(input_data):
+    """This function scales the features in a robust way """
+    scaler = preprocessing.RobustScaler()
+    scaler.fit(input_data)
+    scaled_data = scaler.transform(input_data)
+    
+    return (scaled_data)
