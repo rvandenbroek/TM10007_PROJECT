20094dca3610f4e7d6328ae563c211f9e3a25981
joycerijs <j.rijs@student.tudelft.nl>
2021-03-25 14:25:12 +0100

    Added functions

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello World')
+import pandas as pd
+import os
+import numpy as np
+
+from brats.load_data import load_data
+from preprocessing import imputation
+from preprocessing import robust_scaler
+from split import split_train_test
+
+print('lets go')
+data = load_data()
+data_train, data_test, labels_train, labels_test = split_train_test(data)
+#scaled_data = robust_scaler(data_train)
+#imputed_data = imputation(data_train)
+#print(imputed_data)
+print(data_train[1])
