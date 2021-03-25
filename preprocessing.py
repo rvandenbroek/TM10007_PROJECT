b404cdfe265c0238e870e730fd2d7c1220757b36
joycerijs <j.rijs@student.tudelft.nl>
2021-03-25 13:44:55 +0100

    Added imputation
    Co-authored-by: rvandenbroek <rvandenbroek@users.noreply.github.com>

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,7 @@
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np 
+from sklearn.impute import KNNImputer
 
 def robust_scaler(input_data):
     """This function scales the features in a robust way """
@@ -12,12 +13,23 @@ def robust_scaler(input_data):
     
     return (scaled_data)
 
-# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-# # load dataset into Pandas DataFrame
-# df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width', 'target'])
-# df.pop('target')
-# input_data = np.array(df)
-# print(input_data)
+def imputation(data_frame):
+    '''With this function, the missing features are imputed with KNN'''
+    imputer = KNNImputer(n_neighbors=2, weights="uniform")
+    imputed = imputer.fit_transform(data_frame)
 
-# scaled_data = robust_scaler(input_data)
-# print(scaled_data)
+    return imputed
+
+
+nan = np.nan
+X = [[1, 2, nan], [3, 4, 3], [nan, 6, 5], [8, 8, 7]]
+
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# load dataset into Pandas DataFrame
+df = pd.read_csv(url, names=['sepal length', 'sepal width', 'petal length', 'petal width', 'target'])
+df.pop('target')
+input_data = X
+imputed_data = imputation(input_data)
+scaled_data = robust_scaler(imputed_data)
+print(scaled_data)
