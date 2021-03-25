eff4f978e1756504cf1ab8d8b9d3db904953674a
Edwin Klinkenberg <edwin.klinkenberg@gmail.com>
2021-03-25 11:14:32 +0100

    ?

--- scaler.py
+++ scaler.py
@@ -1,10 +1,16 @@
-!pip install sklearn numpy matplotlib
 # General packages
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets as ds
 import seaborn
+
+from sklearn import model_selection
+from sklearn import metrics
+from sklearn import feature_selection 
+from sklearn import preprocessing
+from sklearn import neighbors
+from sklearn import svm
 
 
 # Scale the dataset
