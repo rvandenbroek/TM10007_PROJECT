5f4b8a97ecfbfdd6da33242b3392f2da01d295d9
joycerijs <j.rijs@student.tudelft.nl>
2021-03-26 16:05:41 +0100

    Doorbraak!! data bevat nu alleen getallen en NaN's

--- main.py
+++ main.py
@@ -6,35 +6,19 @@ from brats.load_data import load_data
 from preprocessing import imputation
 from preprocessing import robust_scaler
 from split import split_train_test
-from PCA import PCA
-
-print('lets go')
+from PCA import PCA_algorithm
+from cross_validation import RFE
 
 data = load_data()
 data_train, data_test, labels_train, labels_test = split_train_test(data)
 
 #data_train=data_train.astype(float)
 #data_train = list(pd.to_numeric(list(data_train), errors='coerce'))
-print(data_train)
-print('tot hier werkt het')
-#data_train= list(data_train)
+
+data_train = data_train.replace(r'#DIV/0!', np.nan, regex=True)
+data_train = data_train.replace([np.inf, -np.inf], np.nan, regex=True)
 
 imputed_data = imputation(data_train)
-#scaled_data = robust_scaler(data_train)
-#print(scaled_data)
-
-#print(imputed_data)
-#print(data_train[1])
-
-#laatste_rij=data_train[1]
-#print(laatste_rij)
-#print(f'laatste = {laatste_rij[-1]}')
-#print(type(laatste_rij[-1]))
-
-print('klaar')
-pca_data = PCA(data_train)
-#imputed_data = imputation(data_train)
-#print(imputed_data)
-
-print(data_train[1])
-
+print(f'imputed data={imputed_data}')
+scaled_data = robust_scaler(imputed_data)
+print(f'scaled data = {scaled_data}')
