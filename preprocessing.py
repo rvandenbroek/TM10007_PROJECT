4aa5c1747bf9008b5f844739950d26bf7b00ee74
joycerijs <j.rijs@student.tudelft.nl>
2021-04-01 10:34:28 +0200

    changed dropnan

--- preprocessing.py
+++ preprocessing.py
@@ -7,25 +7,25 @@ from sklearn.impute import KNNImputer
 
 
 def dropnan(data, threshold):
-    # drop the features with more than threshold NaN's    
+    '''With this function, features with more than threshold amount of NaN's will be dropped.'''
+    # replace '#DIV/0!' and infinity values with NaN
+    data = data.replace(r'#DIV/0!', np.nan, regex=True)
+    data = data.replace([np.inf, -np.inf], np.nan, regex=True)
+    # drop the features with more than threshold NaN's
     data = data.dropna(thresh=threshold, axis=1)
     return data
+
 
 def imputation(train_data, test_data):
     '''With this function, features with more than threshold amount of numbers will be dropped. 
     If this amount is below the threshold, the missing features are imputed with KNN'''
-    # replace '#DIV/0!' and infinity values with NaN
-    train_data = train_data.replace(r'#DIV/0!', np.nan, regex=True)
-    train_data = train_data.replace([np.inf, -np.inf], np.nan, regex=True)
-    test_data = test_data.replace(r'#DIV/0!', np.nan, regex=True)
-    test_data = test_data.replace([np.inf, -np.inf], np.nan, regex=True)
-
     # impute the still existing NaN's 
     imputer = KNNImputer(n_neighbors=2, weights="uniform")
     imputed_train = imputer.fit_transform(train_data)
     imputed_test = imputer.transform(test_data)
 
     return imputed_train, imputed_test
+
 
 def robust_scaler(train_data, test_data):
     """This function scales the features in a robust way """
