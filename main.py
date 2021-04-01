f775a28ff7be4980ad05dc6010b248d75c25a326
rvandenbroek <r.b.vandenbroek@student.tudelft.nl>
2021-04-01 10:27:42 +0200

    volgorde split en remove nan veranderd

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from preprocessing import dropnan
 
 print('start')
 data = load_data()
-data_train, data_test, labels_train, labels_test = split_train_test(data)
 threshold = math.floor(len(data)*0.5)
 data = dropnan(data, threshold)
+data_train, data_test, labels_train, labels_test = split_train_test(data)
+
 imputed_train, imputed_test = imputation(data_train, data_test)
 scaled_train, scaled_test = robust_scaler(imputed_train, imputed_test)
 print('done')
