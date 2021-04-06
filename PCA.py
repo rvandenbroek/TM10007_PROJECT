d50c18127e9f1743a945846d4d615d8bbe19ebe7
joycerijs <j.rijs@student.tudelft.nl>
2021-04-06 12:03:25 +0200

    PCA weer geupdate, stond nog een klein foutje in

--- PCA.py
+++ PCA.py
@@ -12,11 +12,12 @@ from cross_validation import rfe
 from preprocessing import dropnan
 from sklearn import decomposition 
 
+
 def PCA_algorithm(X_train_scaled, X_test_scaled):
     pca = decomposition.PCA(n_components=3)
     pca.fit(X_train_scaled)
     variance = pca.explained_variance_ratio_
-    cumvariance = variance.cumsum())
+    cumvariance = variance.cumsum()
     X_train_pca = pca.transform(X_train_scaled)
     X_test_pca = pca.transform(X_test_scaled)
     return X_train_pca, X_test_pca
@@ -34,32 +35,28 @@ def PCA_algorithm(X_train_scaled, X_test_scaled):
 # X_train_pca, X_test_pca = PCA_algorithm(scaled_train, scaled_test)
 # print(f'X_train: {(X_train_pca.shape)}')
 
-# names = ['PC 1', 'PC 2', 'PC 3', 'PC 4']
+# names = ['PC 1', 'PC 2', 'PC 3']
 # X_train_df = pd.DataFrame(data=X_train_pca, columns=names)
 # X_train_df['labels'] = labels_train
 # X_train_df1 = X_train_df[X_train_df['labels'] == 'GBM']
 # X_train_df2 = X_train_df[X_train_df['labels'] == 'LGG']
 
-# fig, axs = plt.subplots(2, 2)
-# axs[0, 0].boxplot(x=X_train_df1['PC 1'])
-# axs[0, 0].set_title('PC 1 GBM')
-# axs[0, 1].boxplot(x=X_train_df1['PC 2'])
-# axs[0, 1].set_title('PC 2 GBM')
-# axs[1, 0].boxplot(x=X_train_df1['PC 3'])
-# axs[1, 0].set_title('PC 3 GBM')
-# axs[1, 1].boxplot(x=X_train_df1['PC 4'])
-# axs[1, 1].set_title('PC 4 GBM')
+# fig, axs = plt.subplots(1, 3)
+# axs[0].boxplot(x=X_train_df1['PC 1'])
+# axs[0].set_title('PC 1 GBM')
+# axs[1].boxplot(x=X_train_df1['PC 2'])
+# axs[1].set_title('PC 2 GBM')
+# axs[2].boxplot(x=X_train_df1['PC 3'])
+# axs[2].set_title('PC 3 GBM')
 # plt.show()
 
-# fig, axs = plt.subplots(2, 2)
-# axs[0, 0].boxplot(x=X_train_df2['PC 1'])
-# axs[0, 0].set_title('PC 1 LGG')
-# axs[0, 1].boxplot(x=X_train_df2['PC 2'])
-# axs[0, 1].set_title('PC 2 LGG')
-# axs[1, 0].boxplot(x=X_train_df2['PC 3'])
-# axs[1, 0].set_title('PC 3 LGG')
-# axs[1, 1].boxplot(x=X_train_df2['PC 4'])
-# axs[1, 1].set_title('PC 4 LGG')
+# fig, axs = plt.subplots(1, 3)
+# axs[0].boxplot(x=X_train_df2['PC 1'])
+# axs[0].set_title('PC 1 LGG')
+# axs[1].boxplot(x=X_train_df2['PC 2'])
+# axs[1].set_title('PC 2 LGG')
+# axs[2].boxplot(x=X_train_df2['PC 3'])
+# axs[2].set_title('PC 3 LGG')
 # plt.show()
 
 # Dimensionality reduction
